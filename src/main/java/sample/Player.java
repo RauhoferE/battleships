f07930c4276fdd50c39aeb3aa21ef1cc11fc3a78
Emre <emre.rauhofer@gmail.com>
2025-12-05 14:39:00 +0100

    Removed Unused Class

--- src/main/java/sample/Player.java
+++ src/main/java/sample/Player.java
@@ -1,5 +1,7 @@
 package sample;
 
+import javafx.geometry.Pos;
+
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -8,21 +10,21 @@ public class Player
 {
     Field area = new Field();
 
-    private ArrayList<AttackPositions> attackpositions = new ArrayList<>();
+    private ArrayList<Position> attackpositions = new ArrayList<>();
 
     private ArrayList<AIsave> AIsave = null;
 
     /*SaveAttack speichert alle Attacken in die Arraylist*/
     public void SaveAttack(int x, int y)
     {
-        this.attackpositions.add(new AttackPositions(x, y));
+        this.attackpositions.add(new Position(x, y));
     }
 
     /*Wir verhindern doppelten Angriff. Wir schauen, mit der foreach Schleife, ob die Übergebenen x,y von attackPossible
     schon in einer der gespeicherten Stellen in unserer ArrayList attackpositions enthalten ist.*/
     boolean attackPossible(int x, int y)
     {
-        for (AttackPositions a : this.attackpositions)
+        for (Position a : this.attackpositions)
         {
             if ((a.getX() == x) && (a.getY() == y))
             {
